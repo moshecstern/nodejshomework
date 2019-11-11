471598597afd408404d95bb1439384ad19076cd0
Moshe Stern <moshecstern@gmail.com>
2019-11-11 14:40:27 -0500

    bugs worked out, it works

--- liri.js
+++ liri.js
@@ -185,10 +185,10 @@ switch (command) {
         fs.readFile("random.txt", "utf8", function (error, data) {
 
             // If the code experiences any errors it will log the error to the console.
-            if (error) {
-                return console.log(error);
-            }
-            // console.log(data);
+            // if (error) {
+            //     return console.log(error);
+            // }
+            console.log(data);
 
             // Then split it by commas (to make it more readable)
             var dataArr = data.split(",");
